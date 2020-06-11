1b939bc9336606d13356b63577c06f585f750414
Đoàn Cảnh Phong <54398095+phongdoan2306@users.noreply.github.com>
2020-06-11 09:26:58 +0700

    create server
    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+ 
+io.on('connection', (socket) => {
+ 
+  socket.on('set-name', (name) => {
+    socket.username = name;
+  });
+   
+  socket.on('send-message', (message) => {
+    io.emit('message', {msg: message.text, sentby: message.sentby, createdAt: new Date()});    
+  });
+});
+ 
+var port = process.env.PORT || 3000;
+ 
+server.listen(port, function(){
+   console.log('listening in http://localhost:' + port);
+});
